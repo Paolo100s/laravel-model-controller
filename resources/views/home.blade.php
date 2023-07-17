b162626b9f5f99241ff9c0c1894cd6203910b2ec
PaoloCentoscudi <paolocentoscudi@gmail.com>
2023-07-17 15:50:34 +0200

    db error

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main-layout')
+
+@section('content')
+    <ul>
+        @foreach ($movies as $movie)
+            <li>
+                {{ $movie['title'] }}
+            </li>
+        @endforeach
+    </ul>
+@endsection
